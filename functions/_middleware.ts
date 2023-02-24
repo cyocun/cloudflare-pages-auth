5c493eef1efefa4c0d976d72c1b5d438c2b9351d
cyocun <cyocun@gmail.com>
2023-02-24 17:33:01 +0900

    aaaa

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,21 +2,23 @@ import { CFP_ALLOWED_PATHS } from './constants';
 import { getCookieKeyValue } from './utils';
 import { getTemplate } from './template';
 
+const CFP_PASSWORD = "password";
+
 export async function onRequest(context: {
   request: Request;
   next: () => Promise<Response>;
-  env: { CFP_PASSWORD?: string };
+  // env: { CFP_PASSWORD?: string };
 }): Promise<Response> {
-  const { request, next, env } = context;
+  const { request, next } = context;
   const { pathname, searchParams } = new URL(request.url);
   const { error } = Object.fromEntries(searchParams);
   const cookie = request.headers.get('cookie') || '';
-  const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
+  const cookieKeyValue = await getCookieKeyValue(CFP_PASSWORD);
 
   if (
     cookie.includes(cookieKeyValue) ||
     CFP_ALLOWED_PATHS.includes(pathname) ||
-    !env.CFP_PASSWORD
+    !CFP_PASSWORD
   ) {
     // Correct hash in cookie, allowed path, or no password set.
     // Continue to next middleware.
--- functions/cfp_login.ts
+++ functions/cfp_login.ts
@@ -1,20 +1,21 @@
 import { CFP_COOKIE_MAX_AGE } from './constants';
 import { sha256, getCookieKeyValue } from './utils';
+const CFP_PASSWORD = 'password';
 
 export async function onRequestPost(context: {
   request: Request;
-  env: { CFP_PASSWORD?: string };
+  // env: { CFP_PASSWORD?: string };
 }): Promise<Response> {
-  const { request, env } = context;
+  const { request } = context;
   const body = await request.formData();
   const { password, redirect } = Object.fromEntries(body);
   const hashedPassword = await sha256(password.toString());
-  const hashedCfpPassword = await sha256(env.CFP_PASSWORD);
+  const hashedCfpPassword = await sha256(CFP_PASSWORD);
   const redirectPath = redirect.toString() || '/';
 
   if (hashedPassword === hashedCfpPassword) {
     // Valid password. Redirect to home page and set cookie with auth hash.
-    const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
+    const cookieKeyValue = await getCookieKeyValue(CFP_PASSWORD);
 
     return new Response('', {
       status: 302,
